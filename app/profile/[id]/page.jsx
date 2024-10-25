fb09b649b449b82e0cda5d1abd640a5e2c647951
Crimson Tiangco <cftiangco@gmail.com>
2024-10-25 18:07:14 +0800

    before deployment

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -6,37 +6,15 @@ import { useRouter } from 'next/navigation'
 
 import Profile from '@components/Profile'
 
-function MyProfile() {
-
+function MyProfile({params}) {  
     const router = useRouter();
-    
     const [posts, setPosts] = useState([]);
     const {data: session} = useSession();
-
-    const handleEdit = (post) => {
-        router.push(`/update-prompt?id=${post._id}`)
-    }
-
-    const handleDelete = async (post) => {
-        const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
-
-        if(hasConfirmed) {
-            try {
-                await fetch(`/api/prompt/${post._id.toString()}`, {
-                    method: 'DELETE'
-                })
-
-                const filteredPost = posts.filter((p) => p._id !== post._id);
-                setPosts(filteredPost);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-    }
+    const {id} = params; // user id
 
     useEffect(() => {
         const fetchPost = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/posts`);
+            const response = await fetch(`/api/users/${id}/posts`);
             const data = await response.json();
             setPosts(data);
         }
@@ -45,11 +23,9 @@ function MyProfile() {
 
     return (
         <Profile 
-            name="My"
-            desc="Welcome to your personalize profile page!"
+            name="Other user"
+            desc="Welcome to other profile"
             data={posts}
-            handleEdit={handleEdit}
-            handleDelete={handleDelete}
         />
     )
 }
